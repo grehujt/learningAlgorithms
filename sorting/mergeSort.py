e561b0cc12977d92a2e8ed843158baadbae1e019
grehujt <grehujt@users.noreply.github.com>
2018-03-15 12:29:35 +0800

    Update mergeSort.py

--- sorting/mergeSort.py
+++ sorting/mergeSort.py
@@ -1,12 +1,9 @@
 def mergeSort(seq):
     mid = len(seq) // 2
     l1, l2 = seq[:mid], seq[mid:]
-    if len(l1) > 1:
-        l1 = mergeSort(l1)
-    if len(l2) > 1:
-        l2 = mergeSort(l2)
+    if len(l1) > 1: l1 = mergeSort(l1)
+    if len(l2) > 1: l2 = mergeSort(l2)
     tmp = []
     while l1 and l2:
         tmp.append(l1.pop() if l1[-1] > l2[-1] else l2.pop())
-    tmp.reverse()
-    return (l1 or l2) + tmp
+    return (l1 or l2) + tmp[::-1]
