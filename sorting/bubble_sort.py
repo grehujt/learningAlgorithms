9840df0384ef3e67ff7bb3cb92e1d2e433af8484
grehujt <grehujt@users.noreply.github.com>
2018-03-15 16:28:15 +0800

    Update bubble_sort.py

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,6 +1,9 @@
 def bubble_sort(arr):
     for i in range(len(arr)-1, -1, -1):
+        swapped = False
         for j in range(0, i):
             if arr[j+1] < arr[j]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if not swapped: break
     return arr
