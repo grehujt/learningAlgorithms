8777dd04df73b02617335592402d21772b944fbf
grehujt <grehujt@users.noreply.github.com>
2018-03-20 15:47:49 +0800

    Update quickSort.py

--- sorting/quickSort.py
+++ sorting/quickSort.py
@@ -1,7 +1,4 @@
-def quickSort(arr):
-    def partition(arr):
-        p, seq = arr[0], arr[1:]
-        return [x for x in seq if x<=p], p, [x for x in seq if x>p]
-    if len(arr) <= 1: return arr
-    lo, p, hi = partition(arr)
-    return quickSort(lo) + [p] + quickSort(hi)
+def quick_sort(arr):
+    if len(arr) < 2: return arr
+    p, seq = arr[0], arr[1:]
+    return quick_sort([x for x in seq if x <= p]) + [p] + quick_sort([x for x in seq if x > p])
